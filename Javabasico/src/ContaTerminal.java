1c3cfd11e2d24ad603e318f6d8435d4e0a602533
Kauan Martins da Silva <martinskauan27@gmail.com>
2024-07-18 21:58:55 -0300

    Conta Banco

--- Javabasico/src/ContaTerminal.java
+++ Javabasico/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scan = new Scanner(System.in);
+        int saldo = 238;
+
+        System.out.println("Digite seu nome");
+        String nome = scan.next();
+
+        System.out.println("digite o Numero da sua conta");
+        int numeroConta = scan.nextInt(0);
+
+        System.out.println("digite a agencia");
+        int agencia = scan.nextInt();
+
+        System.out.println("Olá"+ nome + ", obrigado por criar uma conta em nosso banco, sua agência é"+ agencia + " conta"+ numeroConta + "e seu saldo já está disponível para saque");
     }
 }
